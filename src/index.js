f3510726aa670821fafa0878c55605b8d14d0937
liyann <liyannan99@gmail.com>
2017-07-15 12:44:26 +0800

    排除了dist目录

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ function component() {
 
   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
   element.innerHTML = _.join(['Hello', 'webpack', 'are you ok?'], ' ');
-  //todo
+  //TODO：哈哈
   return element;
 }
-
+console.log('hello')
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,22 @@
 var path = require('path');
-const webpack = require('webpack'); //访问内置的插件
+const webpack = require('webpack'); //to access built-in plugins
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: '[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+    sourceMapFilename: '[file].map'
   },
   plugins: [
-    new HtmlWebpackPlugin({ template: './src/index.html' })
-  ]
+    new webpack.optimize.UglifyJsPlugin({sourceMap:true}),
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    //Plugin生成sourcemap
+  //   new webpack.SourceMapDevToolPlugin({
+  //     filename: '[name].js.map',
+  //     exclude: ['vendor.js']
+  //   })
+  ],
+  devtool: 'source-map'
 };
